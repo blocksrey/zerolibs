ab04c0ff625e8c6c165b7ad07e382e4ec0907a31
Jeffrey Skinner <Jeff@Pyusoft.com>
2022-06-26 17:38:24 -0700

    ok

--- maths.h
+++ maths.h
@@ -3,129 +3,83 @@
 
 #include "prim.h"
 
-#define ET  0.7853982
-#define QT  1.5707963
-#define PI  3.1415927
-#define IP  0.3183099
-#define TAU 6.2831853
-#define E   2.7182818
-#define RT  1.4142136
-#define PHI 1.6180340
-#define IPH 0.6180340
+#define ET  0.7853982f
+#define QT  1.5707963f
+#define PI  3.1415927f
+#define IP  0.3183099f
+#define TAU 6.2831853f
+#define E   2.7182818f
+#define RT  1.4142136f
+#define PHI 1.6180340f
+#define IPH 0.6180340f
+#define MAN 0x007fffff
 
 #define MIN(a, b) (a < b ? a : b)
 #define MAX(a, b) (a > b ? a : b)
 #define CLAMP(n, l, h) MAX(l, MIN(n, h))
 #define ABS(n) (n < 0 ? n : -n)
-#define SQRT(n) (1/ISQRT(n))
-#define SINP(t) COSP(t - QT)
-#define SIN(t) COS(t - QT)
 #define POW(b, e) EXP(LOG(b)*e)
-#define LOG(n) (0.6931472*LOG2(n))
-#define FLOOR(n) (f32)(i32)n
-#define ROUND(n) FLOOR(n + 0.5)
-
-static f32 ATAN(f32 c, f32 s) {
-	f32 o = 0, t;
-
-	if (s < 0)        c  = -c, s  = -s, o -= PI;
-	if (c < 0) t = c, c  =  s, s  = -t, o += QT;
-	if (c < s) t = c, c +=  s, s -=  t, o += ET;
-
-	f32 d = 2/(c*c + s*s);
-	f32 x = d*c*c - 1;
-	f32 y = d*c*s;
-
-	return
-		o +
-		0.3926991 -
-		0.5574841*(x - y) +
-		0.1648119*(x*x - y*y) -
-		0.0584015*(x*x*y - y*y*x);
-}
-
-static f32 COSP(f32 x) {
-	x *= IP;
-	i32 f = (i32)(x + 0.5);
-	f32 n = x - (f32)f;
-	return (1 + f%2*~1)*(1 + (3.65278*n*n - 4.90704)*n*n);
-}
-
-static f32 COS(f32 x) {
-	x *= IP;
-	i32 f = FLOOR(x + 0.5);
-	f32 n = x - (f32)f;
-	return (1 + f%2*~1)*(1 + (3.65278*n*n - 4.90704)*n*n);
-}
+#define LOG(n) (0.6931472f*LOG2(n))
+#define FLOOR(n) (f32)(i32)(n)
+#define ROUND(n) FLOOR(n + 0.5f)
+#define NFIB(n) ROUND(0.4472136f*(POW(PHI, n) - POW(-IPH, n)))
+#define STIR(n) SQRT(2*PI*n)*POW(n/E, n)
 
 static f32 EXP(f32 x) {
-	x = x/1024 + 1;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	x *= x;
-	return x;
+	union {
+		f32 f;
+		i32 i;
+	} u;
+
+	// exp(x) = 2^i*2^f; i = floor(log2(e)*x), 0 <= f <= 1
+	f32 t = x*1.4426950f;
+	i32 l = FLOOR(t);
+	f32 f = t - l;
+
+	u.f = (0.3371894f*f + 0.6576363f)*f + 1.0017248f; // compute 2^f
+	u.i += l << 23; // scale by 2^i
+
+	return u.f;
 }
 
-static f32 LOG2(f32 x)	// compute LOG2(x) by reducing x to [0.75, 1.5)
-{
-	// a*(x-1)^2 + b*(x-1) approximates LOG2(x) when 0.75 <= x < 1.5
-	const f32 a = -0.6296735;
-	const f32 b = +1.466967;
-	f32 signif, fexp;
-	i32 exp;
-	f32 lg2;
-	union { f32 f; u32 i; } ux1, ux2;
-	i32 greater; // really a boolean 
-	/* 
-	 * Assume IEEE representation, which is sgn(1):EXP(8):frac(23)
-	 * representing (1+frac)*2^(exp-127)	Call 1+frac the significand
-	 */
+static f32 LOG2(f32 x) {
+	union {
+		f32 f;
+		i32 i;
+	} a, b;
 
-	// get exponent
-	ux1.f = x;
-	exp = (ux1.i & 0x7F800000) >> 23; 
-	// actual exponent is exp-127, will subtract 127 later
+	a.f = x;
 
-	greater = ux1.i & 0x00400000;	// true if signif > 1.5
-	if (greater) {
-		// signif >= 1.5 so need to divide by 2.	Accomplish this by 
-		// stuffing exp = 126 which corresponds to an exponent of -1 
-		ux2.i = (ux1.i & 0x007FFFFF) | 0x3f000000;
-		signif = ux2.f;
-		fexp = exp - 126;		// 126 instead of 127 compensates for division by 2
-		signif = signif - 1.0;	// <
-		lg2 = fexp + a*signif*signif + b*signi;	// <
-	} else {
-		// get signif by stuffing exp = 127 which corresponds to an exponent of 0
-		ux2.i = (ux1.i & 0x007FFFFF) | 0x3f800000;
-		signif = ux2.f;
-		fexp = exp - 127;
-		signif = signif - 1.0;	// <<--
-		lg2 = fexp + a*signif*signif + b*signi;	// <<--
-	}
-	// lines marked <<-- are common code, but optimize better 
-	//	when duplicated, at least when using gcc
-	return(lg2);
+	b.i = (a.i & MAN) | 0x3f800000;
+	b.f -= 1;
+
+	return ((a.i & 0x7f800000) >> 23) - 127 - 0.6276936f*b.f*b.f + 1.4649446f*b.f;
 }
 
+static f32 SQRT(f32 n) {
+	union {
+		f32 f;
+		i32 i;
+	} u;
 
+	u.f = n;
+	u.i = 0x1fc00000 + (u.i >> 1);
 
+	u.f = 0.5f*(u.f + n/u.f); // Baybylonian #1
+
+	return u.f;
+}
 
 static f32 ISQRT(f32 n) {
 	union {
-		i32 i;
 		f32 f;
-	} u = {.f = n};
+		i32 i;
+	} u;
 
+	u.f = n;
 	u.i = 0x5f3759df - (u.i >> 1);
-	u.f *= 0.5*(3 - n*u.f*u.f);
+
+	u.f *= 0.5f*(3 - n*u.f*u.f); // Newton #1
 
 	return u.f;
 }
@@ -135,16 +89,8 @@ static f32 MOD(f32 x, f32 y) {
 	return x - y*(x < i ? i - 1 : i);
 }
 
-static f32 FACT(f32 x) {
+static inline f32 FACT(f32 x) {
 	return x > 0 ? x*FACT(x - 1) : 1;
-}
-
-static f32 STIR(f32 x) {
-	return SQRT(2*PI*x)*pow(x/E, x);
-}
-
-static u32 NFIB(f32 x) {
-	return ROUND(0.4472136*(POW(PHI, x) - POW(-IPH, x)))
 }
 
 #endif
--- trig.h
+++ trig.h
@@ -3,75 +3,89 @@
 
 #include "maths.h"
 
-/*
-f32 cos(f32 x) {
-	x *= 0.6366198; x += 1;
-	f32 y = mod(x, 2) - 1;
-	return 0.2265329*(1.0017028f - y*y)*(4.4050123f - y*y)*(2 + y - mod(x, 4));
-}
-
-f32 sin(f32 x) {
-	x *= 0.6366198;
-	f32 y = mod(x, 2) - 1;
-	return 0.2265329*(1.0017028f - y*y)*(4.4050123f - y*y)*(2 + y - mod(x, 4));
-}
-*/
-
 #define TAN(x) (SIN(x)/COS(x))
+#define SINP(t) COSP(t - QT)
+#define SIN(t) COS(t - QT)
+
+static f32 COSP(f32 t) {
+	t *= IP;
+	i32 f = (i32)(t + 0.5f);
+	f32 n = t - (f32)f;
+	return (1 + f%2*~1)*(1 + (3.6527833f*n*n - 4.9070450f)*n*n);
+}
+
+static f32 ATAN2(f32 c, f32 s) {
+	f32 o = 0, t;
+
+	if (s < 0)        c  = -c, s  = -s, o -= PI;
+	if (c < 0) t = c, c  =  s, s  = -t, o += QT;
+	if (c < s) t = c, c +=  s, s -=  t, o += ET;
+
+	f32 d = 2/(c*c + s*s);
+	f32 x = d*c*c - 1;
+	f32 y = d*c*s;
+
+	return
+		o +
+		0.3926991f -
+		0.5574841f*(x - y) +
+		0.1648119f*(x*x - y*y) -
+		0.0584015f*(x*x*y - y*y*x);
+}
 
 //inverse
 //[-1, 1]
-f32 ACOS(f32 x) {
+static f32 ACOS(f32 x) {
 	return
-		1.5707963 -
-		1.0469419*x +
-		0.1610680*x*x*x -
-		0.5315243*x*x*x*x*x;
+		1.5707963f -
+		1.0469419f*x +
+		0.1610680f*x*x*x -
+		0.5315243f*x*x*x*x*x;
 }
 
 //[-1, 1]
-f32 ASIN(f32 x) {
+static f32 ASIN(f32 x) {
 	return
-		1.0469419*x -
-		0.1610680*x*x*x +
-		0.5315243*x*x*x*x*x;
+		1.0469419f*x -
+		0.1610680f*x*x*x +
+		0.5315243f*x*x*x*x*x;
 }
 
 //[-1, 1]
-f32 ATAN(f32 x) {
+static f32 ATAN(f32 x) {
 	return
-		0.9959826*x -
-		0.2922813*x*x*x +
-		0.0830216*x*x*x*x*x;
+		0.9959826f*x -
+		0.2922813f*x*x*x +
+		0.0830216f*x*x*x*x*x;
 }
 
 //forward hyperbolic
-f32 COSH(f32 x) {
+static f32 COSH(f32 x) {
 	f32 y = EXP(x);
-	return 0.5*(1/y + y);
+	return 0.5f*(1/y + y);
 }
 
-f32 SINH(f32 x) {
+static f32 SINH(f32 x) {
 	f32 y = EXP(x);
-	return 0.5*(y - 1/y);
+	return 0.5f*(y - 1/y);
 }
 
-f32 TANH(f32 x) {
+static f32 TANH(f32 x) {
 	f32 y = EXP(2*x);
 	return (y - 1)/(y + 1);
 }
 
 //inverse hyperbolic
-f32 ACOSH(f32 x) {
+static f32 ACOSH(f32 x) {
 	return LOG(x + SQRT(x*x - 1));
 }
 
-f32 ASINH(f32 x) {
+static f32 ASINH(f32 x) {
 	return LOG(x + SQRT(x*x + 1));
 }
 
-f32 ATAHN(f32 x) {
-	return 0.5*(LOG(1 + x) - LOG(1 - x));
+static f32 ATAHN(f32 x) {
+	return 0.5f*(LOG(1 + x) - LOG(1 - x));
 }
 
 #endif
